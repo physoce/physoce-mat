87c0752e9a3b441dab36a6cfb275f861bb6e448f
Tom Connolly <tconnolly@mlml.calstate.edu>
2015-10-21 17:10:04 -0700

    added functions for testing significance of correlations

---
+++ stats/rcrit.m
@@ -0,0 +1,30 @@
+function rc = rcrit(sig,nu)
+
+% function rc = rcrit(sig,nu)
+% ---------------------------
+% Critical r (correlation coefficient), given significance level
+% and degrees of freedom.
+%
+% INPUTS:
+% sig - significance level (default 0.05)
+% nu - degrees of freedom (N-2)
+% 
+% OUTPUT:
+% rcrit - critical r value
+% 
+% Values for 0.05 and 0.01 correspond with Appendix E in
+% Emery and Thomson (2004) Data Analysis Methods in Physical 
+% Oceanography
+%
+% Tom Connolly (tconnolly@mlml.calstate.edu)
+
+% set default significance level
+if nargin == 1
+    sig = 0.05
+end
+
+% critical t value
+t = tinv(1 - sig/2,nu);
+
+% critical r value
+rc = t/sqrt(t^2+nu);
